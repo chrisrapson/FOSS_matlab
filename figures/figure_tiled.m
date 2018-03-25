379f9ebf4abd0ad4a82746b7c4e5ff1a05d76162
Chris Rapson <chris.rapson@aut.ac.nz>
2018-03-26 12:24:06 +1300

    use OuterPosition to more accurately tile figures

--- figures/figure_tiled.m
+++ figures/figure_tiled.m
@@ -15,16 +15,16 @@ if nargin<2 || length(figSize(:))~=2
 end
 
 screenSize=get(0,'ScreenSize');%consider using MonitorPositions
-screenSize(1:2)=screenSize(3:4);
-height=screenSize(4)/2*0.86*figSize(2); %0.86 allows padding for window decoration and taskbar
-width=screenSize(3)/3*0.98*figSize(1);  %0.98 allows padding for window decoration
+taskbar_height = 0.04 * screenSize(4);
+height=(screenSize(4) - taskbar_height)/2*figSize(2); %no padding required when using OuterPosition
+width=screenSize(3)/3*figSize(1);  %no padding required when using OuterPosition
 
-figurePositions=[1,                     screenSize(4)/2*1.1,  width,height;  %1
-                screenSize(3)/3*1.01    screenSize(4)/2*1.1,  width,height;  %2
-                screenSize(3)*2/3*1.01  screenSize(4)/2*1.1,  width,height;  %3
-                1,                      1,                    width,height;  %4
-                screenSize(3)/3*1.01,   1,                    width,height;  %5
-                screenSize(3)*2/3*1.01, 1,                    width,height]; %6
+figurePositions=[1,                taskbar_height + height,  width,height;  %1
+                screenSize(3)/3    taskbar_height + height,  width,height;  %2
+                screenSize(3)*2/3  taskbar_height + height,  width,height;  %3
+                1,                 taskbar_height,                width,height;  %4
+                screenSize(3)/3,   taskbar_height,                width,height;  %5
+                screenSize(3)*2/3, taskbar_height,                width,height]; %6
 
 if nargin==0 || isempty(figNumber)
     figHandle=figure;
@@ -33,7 +33,7 @@ else
 end
 
 if isnumeric(figHandle)
-    set(gcf,'position',figurePositions(mod(figHandle-1,6)+1,:))
+    set(gcf,'OuterPosition',figurePositions(mod(figHandle-1,6)+1,:))
 else
-    set(gcf,'position',figurePositions(mod(get(gcf,'Number')-1,6)+1,:))
+    set(gcf,'OuterPosition',figurePositions(mod(get(gcf,'Number')-1,6)+1,:))
 end
