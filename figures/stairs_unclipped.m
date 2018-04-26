102b543ef000f1de08035f8dc533563f7a27a821
Chris Rapson <chris.rapson@aut.ac.nz>
2018-04-27 09:32:20 +1200

    add ability to use longer x than y for stairs_unclipped, and for y to be logical OR numeric

--- figures/stairs_unclipped.m
+++ figures/stairs_unclipped.m
@@ -1,6 +1,9 @@
 function varargout = stairs_unclipped(varargin)
 %wrapper for stairs which doesn't leave off the last point in the vector
 %
+%The routine even allows for x data to be the same size or one element
+%longer than y
+%
 % See also: stairs
 
 arg_pass_through = {};
@@ -8,7 +11,7 @@ x = [];
 if nargin == 1
 	y = varargin{1};
 elseif nargin > 1
-	if isnumeric(varargin{1}) && isnumeric(varargin{2})
+	if isnumeric(varargin{1}) && (isnumeric(varargin{2}) || islogical(varargin{2}))
 		x = varargin{1};
 		y = varargin{2};
 		if nargin > 2
@@ -26,13 +29,17 @@ if size(y,1) > 1
 	for ii = size(y,2):-1:1
 		y(end+1,ii) = y(end,ii);
 	end
-	for ii = size(x,2):-1:1
-		x(end+1,ii) = x(end,ii) + (x(end,ii)-x(end-1,ii));
+	if size(x,1) == size(y,1) -1
+		for ii = size(x,2):-1:1
+			x(end+1,ii) = x(end,ii) + (x(end,ii)-x(end-1,ii));
+		end
 	end
 elseif size(y,2) > 1
 	%y is a row vector (hopefully x is too)
 	y(end+1) = y(end);
-	x(end+1) = x(end) + (x(end)-x(end-1));
+	if length(x) == size(y,2) - 1
+		x(end+1) = x(end) + (x(end)-x(end-1));
+	end
 end
 
 if isempty(x)
