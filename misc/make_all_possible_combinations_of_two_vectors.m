e4c6d69aebeeba7ea5558d67680be855a28b05da
Chris Rapson <chris.rapson@aut.ac.nz>
2018-07-18 14:59:37 +1200

    add reference to a similar function perms

--- misc/make_all_possible_combinations_of_two_vectors.m
+++ misc/make_all_possible_combinations_of_two_vectors.m
@@ -2,6 +2,8 @@ function z=make_all_possible_combinations_of_two_vectors(x,y)
 % output is a 2xN matrix with all possible combinations of the values in the 
 % input vectors. Elements from first vector are in the first row.
 % There is no check for duplicates.
+%
+% See also: perms
 
 %Author: Chris Rapson
 
