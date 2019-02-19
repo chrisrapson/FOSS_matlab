de34dd7797c5397cf6a17e7af036d2d5cde37f4b
Chris Rapson <chris.rapson@aut.ac.nz>
2019-02-19 16:10:17 +1300

    allow handles for figure and axis to be passed to getTitle()

--- figures/make_it_look_nice/getTitle.m
+++ figures/make_it_look_nice/getTitle.m
@@ -1,4 +1,4 @@
-function filename=getTitle();
+function filename=getTitle(fh,axh)
 %extract the title from gca, or if that 
 %fails, use the figure number from gcf.
 %The title can be used for saving/exporting
@@ -6,19 +6,26 @@ function filename=getTitle();
 
 %Author: Chris Rapson
 
-    tmp=get(get(gca,'Title'));
-    if exist('tmp','var') && ~isempty(tmp.String)
-        if iscell(tmp.String)
-            tmp2=[tmp.String{1}];
-            for i=2:length(tmp.String)
-                tmp2=[tmp2,';_',tmp.String{i}];
-            end
-            filename=tmp2;
-        else
-            filename=tmp.String;
-        end
-    else
-        filename=get(gcf, 'Number');
+if nargin < 1 || isempty(fh)
+	fh = gcf;
+end
+if nargin < 2 || isempty(axh)
+	axhs = get(fh,'Children');
+	axh = axhs(1);
+end
+
+tmp=get(get(axh,'Title'));
+if exist('tmp','var') && ~isempty(tmp.String)
+		if iscell(tmp.String)
+				tmp2=[tmp.String{1}];
+				for i=2:length(tmp.String)
+						tmp2=[tmp2,';_',tmp.String{i}];
+				end
+				filename=tmp2;
+		else
+				filename=tmp.String;
+		end
+else
+		filename=get(fh, 'Number');
 %     end
-    end
-return
+end
